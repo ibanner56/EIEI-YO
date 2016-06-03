388e30e96563344a41ea940b002ae78e7de9fb1e
Cristina Buenahora <crisb@seas.upenn.edu>
2016-06-03 15:05:10 -0700

    initial commit

--- config.js
+++ config.js
@@ -3,11 +3,11 @@
  exports.creds = {
  	returnURL: 'http://localhost:3000/auth/openid/return',
  	identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
- 	clientID: '<your app id>',
- 	clientSecret: '<your secret>', // if you are doing code or id_token code
+ 	clientID: '5ca08eed-ae06-4415-bfeb-6a3c646d23a9',
+ 	clientSecret: 'eUBMr%mK1s7O0KDi', // if you are doing code or id_token code
  	skipUserProfile: true, // for AzureAD should be set to true.
  	responseType: 'id_token', // for login only flows use id_token. For accessing resources use `id_token code`
  	responseMode: 'form_post', // For login only flows we should have token passed back to us in a POST
  	//scope: ['email', 'profile'] // additional scopes you may wish to pass
- 	tenantName: 'hypercubeb2c.onmicrosoft.com'
+ 	tenantName: 'hypercubeb2c.onmicrosoft.com',
  	};
